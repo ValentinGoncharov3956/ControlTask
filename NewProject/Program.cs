f7749ace7d035e9c6f1f99f741a4a6349764ccd4
Valik <valik.seal12@gmail.com>
2023-03-30 21:42:14 +0300

    Create method2

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам.
 
 
-string[] ArrayString = new string[5] { "string", "array", "925", "24", "12" };
+string[] ArrayString = new string[5] {"string", "array", "925", "24", "12"};
 string[] ArrayString2 = new string[ArrayString.Length];
 
 void ArrayCheck(string[] array, string[] array2)
@@ -17,3 +17,12 @@ void ArrayCheck(string[] array, string[] array2)
     }
 }
 
+void PrintArray(string[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");   
+    }
+    Console.WriteLine($"]");;
+}
