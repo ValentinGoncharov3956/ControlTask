7effc94962c059ddda1ff5cf42d6c1b9032b513f
Valik <valik.seal12@gmail.com>
2023-03-30 21:31:02 +0300

    Create array

--- NewProject/Program.cs
+++ NewProject/Program.cs
@@ -1,1 +1,17 @@
 ﻿//Задача. Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам.
+
+
+string[] ArrayString = new string[5] {"string", "array", "925", "24", "12"};
+string[] ArrayString2 = new string[ArrayString.Length];
+int count = 0;
+
+for (int i = 0; i < ArrayString.Length; i++)
+{
+    if (ArrayString[i].Length <= 3)
+    {
+        ArrayString2[count] = ArrayString[i];
+        count++;
+    }
+}
+
+
